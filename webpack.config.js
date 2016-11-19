6c084f271902ce7b03da821277d4d835d60a08ee
Rene M. Paccha <paccha@gmail.com>
2016-11-19 15:52:34 -0600

    setting up production via a separate webpack --production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './app/assets/frontend/main.jsx',
   output: {
-    path: __dirname + "/app/assets/javascripts",
+    path: __dirname + "/public/",
     filename: 'bundle.js'
   },
   resolve: {
